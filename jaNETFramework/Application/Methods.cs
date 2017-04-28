b7a7fc12ddbf3215adfbcd0fbba495397f30c9b1
jambel <jambel@gmail.com>
2017-04-28 19:14:43 +0300

    Change versioning and remove logger from weather

--- jaNETFramework/Application/Methods.cs
+++ jaNETFramework/Application/Methods.cs
@@ -34,7 +34,7 @@ namespace jaNET.Environment
     {
         public static Methods Instance { get { return Singleton<Methods>.Instance; } }
 
-        public const string AssemblyVersion = "0.3.0.40";
+        public const string AssemblyVersion = "0.3.1.41";
 
         public string GetCopyright {
             get {
